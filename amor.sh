3b2be8659ddddcb0b59532c4c55186d6827ba2f2
Arcangel96 <105159772+Arcangel96@users.noreply.github.com>
2024-12-12 20:13:20 -0500

    Add files via upload

---
+++ amor.sh
@@ -0,0 +1,84 @@
+#!/bin/bash
+
+apt install sl && sl
+pkg install cmatrix
+
+clear
+
+echo -e "\033[0;34m"
+
+echo "TE AMOOOO MUCHOOO MI ANGELITA"
+
+▀██▀─▄███▄─▀██─██▀██▀▀█
+─██─███─███─██─██─██▄█
+─██─▀██▄██▀─▀█▄█▀─██▀█
+▄██▄▄█▀▀▀─────▀──▄██▄▄█
+
+echo -e "\033[0;32m"
+
+opciones=("Te amo 1" "Te amo 2" "Te amo 3" "Salir")
+
+echo "Menú de opciones"
+for i in "${!opciones[@]}"; do
+  echo "$i) ${opciones[$i]}"
+done
+
+echo -e "\033[0;31m"
+
+read -p "Seleccione una opción: " opcion
+
+case $opcion in
+
+  1) echo  " Mi amor más preciado,
+
+Quiero que sepas que eres la razón por la que mi corazón late fuerte, la persona que hace que mi vida tenga sentido. Eres la luz que ilumina mi camino, la estrella que guía mis pasos, la brisa que refresca mi alma.
+
+Eres la persona más hermosa que he conocido, no solo por tu belleza exterior, sino por tu belleza interior. Tu corazón es puro y generoso, tu espíritu es fuerte y valiente, tu amor es incondicional y desinteresado.
+
+Quiero que sepas que te amo más que a nada en este mundo. Te amo por quién eres, por tu bondad, tu inteligencia, tu belleza. Te amo por la forma en que me haces sentir, por la forma en que me miras, por la forma en que me tocas.
+
+Quiero que sepas que nunca te voy a dejar sola. Estaré siempre aquí para ti, para apoyarte, para protegerte, para amarte. Quiero que sepas que eres mi todo, mi razón de ser, mi vida.
+
+No importa lo que pase, no importa lo que suceda, siempre estaré aquí para ti. Te amo, te amo, te amo, más que ayer, pero menos que mañana.
+
+Eres mi alma gemela, mi media naranja, mi compañera de vida. Eres la persona que hace que mi vida sea completa, la persona que hace que mi corazón sea feliz.
+
+Quiero que sepas que te amo, te amo, te amo, por siempre y para siempre. Eres la razón por la que vivo, la razón por la que respiro, la razón por la que late mi corazón.
+
+Te amo más que a nada en este mundo, más que a mi propia vida. Eres mi todo, mi razón de ser, mi vida. Quiero que sepas que siempre estaré aquí para ti, para amarte, para protegerte, para apoyarte.
+
+Te amo, mi amor, te amo.  te amo mi amor aqui te dejo este link:  https://gifft.me/es/o/l/esr4e43r9txanob5i6uef2aj ";;
+  2) echo " Mi amor, mi corazón late solo por ti. Quiero que sepas que estás siempre en mi mente y en mi corazón. Aunque estemos lejos, mi amor por ti no disminuye, sino que crece cada día más.
+
+No quiero que te sientas sola, porque estoy aquí para ti. Estoy aquí para escucharte, para abrazarte, para amarte. Estoy aquí para hacer que te sientas segura, para hacer que te sientas amada.
+
+Mi amor, eres la razón por la que vivo. Eres la razón por la que respiro. Eres la razón por la que late mi corazón. Sin ti, mi vida no tendría sentido.
+
+Quiero que sepas que te amo más que a nada en este mundo. Te amo por quién eres, por tu belleza, por tu inteligencia, por tu bondad. Te amo por la forma en que me haces sentir, por la forma en que me miras, por la forma en que me tocas.
+
+Mi amor, no te preocupes por nada. Estoy aquí para ti, para protegerte, para amarte. Estoy aquí para hacer que te sientas feliz, para hacer que te sientas amada.
+
+Quiero que sepas que te amo, te amo, te amo, por siempre y para siempre. Eres mi todo, mi razón de ser, mi vida. Sin ti, no soy nada.
+
+Mi amor, te amo más que ayer, pero menos que mañana. Te amo más con cada paso que doy, más con cada respiración que hago. Te amo más con cada latido de mi corazón.
+
+Quiero que sepas que estoy aquí para ti, para siempre. Estoy aquí para amarte, para protegerte, para hacer que te sientas feliz. Estoy aquí para ti, mi amor.  Te amo mi amor aqui hay una sorpresa jiji:  https://www.mediafire.com/file/79l6jxeddhvhxy0/slidesgo-un-retrato-del-amor-celebrando-a-mi-novia-a-traves-de-palabras-y-arte-20241213001053R4tx.pdf/file";;
+  3) echo "Mi amor más preciado,
+
+Quiero que sepas que eres la razón por la que mi corazón late fuerte, la persona que hace que mi vida tenga sentido. Eres la luz que ilumina mi camino, la estrella que guía mis pasos, la brisa que refresca mi alma.
+
+Eres la persona más hermosa que he conocido, no solo por tu belleza exterior, sino por tu belleza interior. Tu corazón es puro y generoso, tu espíritu es fuerte y valiente, tu amor es incondicional y desinteresado.
+
+Quiero que sepas que te amo más que a nada en este mundo. Te amo por quién eres, por tu bondad, tu inteligencia, tu belleza. Te amo por la forma en que me haces sentir, por la forma en que me miras, por la forma en que me tocas.
+
+Quiero que sepas que nunca te voy a dejar sola. Estaré siempre aquí para ti, para apoyarte, para protegerte, para amarte. Quiero que sepas que eres mi todo, mi razón de ser, mi vida.
+
+No importa lo que pase, no importa lo que suceda, siempre estaré aquí para ti. Te amo, te amo, te amo, más que ayer, pero menos que mañana.
+
+Eres mi alma gemela, mi media naranja, mi compañera de vida. Eres la persona que hace que mi vida sea completa, la persona que hace que mi corazón sea feliz.
+
+Quiero que sepas que te amo, te amo, te amo, por siempre y para siempre. otro regalito amor https://www.mediafire.com/file/2xkxg8vmlcjrpvc/base.apk/file  ";;
+  4) echo "Saliendo..." ; exit 0;;
+  *) echo "Opción inválida";;
+esac
+
